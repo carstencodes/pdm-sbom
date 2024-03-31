698a8eebba2720100b110cdb8740542f19665345
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2024-03-31 21:09:58 +0200

    refactor: Update cyclone dx to dag/project
    Project and DAG are used to build the CycloneDX Export

--- src/pdm_sbom/sbom/cyclonedx.py
+++ src/pdm_sbom/sbom/cyclonedx.py
@@ -9,32 +9,36 @@
 # This file is published using the MIT license.
 # Refer to LICENSE for more information
 #
-from typing import IO, AnyStr, Iterable, Mapping, cast
+from collections.abc import Sequence
+from typing import IO, AnyStr, Iterable, Mapping, cast, Optional, ClassVar, Union, Final
+from uuid import UUID, uuid5, NAMESPACE_URL
 
 from cyclonedx.model import (  # type: ignore
-    LicenseChoice,
     OrganizationalContact,
     OrganizationalEntity,
-    Tool,
+    Tool, ExternalReference, ExternalReferenceType, XsUri, HashType,
 )
-from cyclonedx.model.bom import Bom  # type: ignore
-from cyclonedx.model.bom_ref import BomRef  # type: ignore
-from cyclonedx.model.component import (  # type: ignore
+from cyclonedx.model.bom import Bom, BomMetaData
+from cyclonedx.model.bom_ref import BomRef
+from cyclonedx.model.component import (
     Component,
     ComponentScope,
     ComponentType,
 )
-from cyclonedx.output import (  # type: ignore
+from cyclonedx.model.dependency import Dependency
+from cyclonedx.model.license import LicenseExpression, DisjunctiveLicense
+from cyclonedx.output import (
     OutputFormat,
     SchemaVersion,
 )
-from cyclonedx.output import get_instance as get_output_instance  # type: ignore
-from cyclonedx.parser import BaseParser  # type: ignore
-from packageurl import PackageURL  # type: ignore
+from cyclonedx.output import make_outputter as get_output_instance
+from packageurl import PackageURL
 
 from .base import ExporterBase, FormatAndVersionMixin, ToolInfo
-from .data import Component as InnerComponent
-from .data import ComponentUsage, Project
+from ..dag import UsageKind, Graph
+from ..project import ProjectInfo, AuthorInfo, ComponentInfo, LicenseInfo, create_self_info, create_pdm_info
+from ..project.tools import create_module_info
+from datetime import datetime
 
 
 class CycloneDXExporter(ExporterBase, FormatAndVersionMixin):
@@ -48,11 +52,18 @@ class CycloneDXExporter(ExporterBase, FormatAndVersionMixin):
         "1.2": SchemaVersion.V1_2,
         "1.3": SchemaVersion.V1_3,
         "1.4": SchemaVersion.V1_4,
+        "1.5": SchemaVersion.V1_5,
     }
 
     SUPPORTED_FILE_FORMATS: frozenset[str] = frozenset(_EXTENSIONS.keys())
     SUPPORTED_VERSIONS: frozenset[str] = frozenset(_VERSIONS.keys())
+    DEFAULT_FILE_FORMAT: Final[str] = "json"
+    DEFAULT_FILE_VERSION: Final[str] = "1.5"
     FORMAT_NAME: str = "cyclonedx"
+    SHORT_FORMAT_CODE: str = "c"
+    FORMAT_DESCRIPTION = f"CycloneDX file format - "\
+                         f"supported versions: {', '.join(SUPPORTED_VERSIONS)} - "\
+                         f"supported formats: {', '.join(SUPPORTED_FILE_FORMATS)}"
 
     def __init__(self, project, *tools: ToolInfo) -> None:
         ExporterBase.__init__(self, project, *tools)
@@ -63,20 +74,8 @@ class CycloneDXExporter(ExporterBase, FormatAndVersionMixin):
         return self._EXTENSIONS[self.file_format][0]
 
     def export(self, stream: IO[AnyStr]) -> None:
-        parser: _PdmExportParser = _PdmExportParser(self.project)
-        bom: Bom = Bom.from_parser(parser=parser)
-        for tool in self.tools:
-            bom.metadata.tools.add(
-                Tool(
-                    vendor=tool.vendor,
-                    name=tool.name,
-                    version=tool.version,
-                )
-            )
-
-        bom.metadata.authors = parser.authors_to_oc(self.project.author)
-        bom.metadata.manufacture = parser.project_to_oe(self.project)
-        bom.metadata.component = parser.component_to_cyclonedx(self.project)
+        builder: _PdmBuilder = _PdmBuilder(self.graph)
+        bom: Bom = builder.build()
 
         if not bom.validate():
             pass  # TODO result is always true or an exception
@@ -94,73 +93,154 @@ class CycloneDXExporter(ExporterBase, FormatAndVersionMixin):
         stream.write(data_to_write)
 
 
-class _PdmExportParser(BaseParser):
-    def __init__(self, project: Project) -> None:
-        super().__init__()
-        scopes: dict[ComponentUsage, ComponentScope | None] = {
-            ComponentUsage.Root: None,
-            ComponentUsage.Direct: ComponentScope.REQUIRED,
-            ComponentUsage.Optional: ComponentScope.OPTIONAL,
-            ComponentUsage.Development: ComponentScope.EXCLUDED,
+class _PdmBuilder:
+    scopes: ClassVar[dict[UsageKind, Optional[ComponentScope]]] = {
+            UsageKind.ROOT: None,
+            UsageKind.REQUIRED: ComponentScope.REQUIRED,
+            UsageKind.OPTIONAL: ComponentScope.OPTIONAL,
+            UsageKind.DEVELOPMENT: ComponentScope.EXCLUDED,
+            UsageKind.UNUSED: ComponentScope.EXCLUDED,
         }
 
-        done: set[InnerComponent] = set()
+    def __init__(self, graph: Graph) -> None:
+        self.__project: ProjectInfo = graph.root_node.project
+        self.__graph: Graph = graph
 
-        for child in project.recurse_project(True, True, False):
-            if child.component in done:
-                continue
-            component = self.component_to_cyclonedx(
-                child.component, scopes[child.usage]
-            )
-            self._components.append(component)
-            done.add(child.component)
+    def build(self) -> Bom:
+        return Bom(
+            components=self.__build_components(),
+            services=(),
+            external_references=self.__get_external_references(),
+            serial_number=self.__get_serial_number(),
+            version=1,
+            metadata=self.__get_metadata(),
+            dependencies=self.__get_dependencies(),
+            vulnerabilities=()
+        )
 
+    @staticmethod
     def authors_to_oc(
-        self, authors: list[tuple[str, str]]
+        authors: Sequence[AuthorInfo]
     ) -> Iterable[OrganizationalContact]:
         for author in authors:
-            name, email = author
-            yield OrganizationalContact(name=name, email=email, phone=None)
+            yield OrganizationalContact(name=author.name, email=author.email, phone=None)
 
-    def project_to_oe(self, project: Project) -> OrganizationalEntity:
+    @staticmethod
+    def project_to_oe(project: ProjectInfo) -> OrganizationalEntity:
         return OrganizationalEntity(
             name=f"{project.name} Authors",
             urls=None,  # TODO
-            contacts=self.authors_to_oc(project.author),
+            contacts=_PdmBuilder.authors_to_oc(project.authors),
         )
 
+    @staticmethod
     def license_to_license_choice(
-        self, license_id: str | None
-    ) -> Iterable[LicenseChoice]:
-        if license_id is not None:
-            yield LicenseChoice(license_expression=license_id)
+        _: LicenseInfo
+    ) -> Iterable[Union[LicenseExpression, DisjunctiveLicense]]:
+        return ()
 
+    @staticmethod
     def component_to_cyclonedx(
-        self, component: InnerComponent, scope: ComponentScope | None = None
+        component: ComponentInfo, scope: Optional[ComponentScope] = None, group: Optional[str] = None
     ) -> Component:
-        purl: PackageURL = PackageURL(
-            type="pypi", name=component.name, version=str(component.version)
-        )
-        dependencies = [self.component_to_bom_ref(c) for c in component.dependencies]
+        purl: PackageURL = component.get_package_url()
 
         result = Component(
             name=component.name,
-            component_type=ComponentType.LIBRARY,  # TODO
+            type=ComponentType.LIBRARY,  # TODO
             bom_ref=purl.to_string(),
-            licenses=self.license_to_license_choice(component.license_id),
+            licenses=_PdmBuilder.license_to_license_choice(component.license),
+            mime_type=None,
             purl=purl,
-            version=str(component.version),
+            version=str(component.resolved_version) if component.resolved else None,
             scope=scope,
+            supplier=None,  # TODO
+            author=None,  # TODO
+            publisher=None,  # TODO
+            group=group,
+            hashes=(),
+            copyright=None,  # TODO
+            external_references=_PdmBuilder.__get_external_references_for(component),
+            properties=(),
+            release_notes=None,
+            cpe=None,
+            swid=None,
+            pedigree=None,
+            components=(),
+            evidence=None,  # TODO
+            modified=False,
         )
-
-        result.dependencies = dependencies
 
         return result
 
-    def component_to_bom_ref(self, component: InnerComponent) -> BomRef:
-        purl = PackageURL(
-            type="pypi",
-            name=component.name,
-            version=str(component.version),
-        )
+    @staticmethod
+    def __component_to_bom_ref(component: ComponentInfo) -> BomRef:
+        purl = component.get_package_url()
         return BomRef(value=purl.to_string())
+
+    @staticmethod
+    def __get_external_references_for(component: ComponentInfo) -> Iterable[ExternalReference]:
+        for file in component.files:
+            yield ExternalReference(
+                comment=None,
+                type=ExternalReferenceType.DISTRIBUTION,
+                url=XsUri(f"cyclonedx-files://{component.name}/files{file.file}"),
+                hashes=(
+                    HashType.from_composite_str(file.hash),
+                )
+            )
+
+    @staticmethod
+    def __get_tools(*tools: ToolInfo) -> Iterable[Tool]:
+        for tool in tools:
+            yield Tool(
+                name=tool.name,
+                vendor=tool.vendor,
+                version=str(tool.version),
+            )
+
+    def __get_metadata(self) -> BomMetaData:
+        return BomMetaData(
+            component=_PdmBuilder.component_to_cyclonedx(self.__project, None, None),
+            tools=_PdmBuilder.__get_tools(
+                create_self_info(),
+                create_pdm_info(),
+                create_module_info("cyclonedx_bom"),
+                create_module_info("cyclonedx_python_lib"),
+            ),
+            authors=_PdmBuilder.authors_to_oc(self.__project.authors),
+            manufacture=_PdmBuilder.project_to_oe(self.__project),
+            supplier=None,
+            licenses=None,  # TODO
+            properties=(),
+            timestamp=datetime.utcnow()
+        )
+
+    def __get_serial_number(self) -> UUID:
+        return uuid5(NAMESPACE_URL, f"cyclonedx://{self.__project.name}/{self.__project.resolved_version}")
+
+    def __build_components(self) -> Iterable[Component]:
+        yield _PdmBuilder.component_to_cyclonedx(self.__project, None, None)
+        for node in self.__graph.nodes:
+            if node == self.__graph.root_node:
+                continue
+            cs: Optional[ComponentScope] = _PdmBuilder.scopes.get(node.usage, None)
+            yield _PdmBuilder.component_to_cyclonedx(node.component, cs, node.group)
+
+    def __get_external_references(self) -> Iterable[ExternalReference]:
+        return _PdmBuilder.__get_external_references_for(self.__project)
+
+    def __get_dependencies(self) -> Iterable[Dependency]:
+        for node in self.__graph.nodes:
+            yield Dependency(
+                ref=_PdmBuilder.__component_to_bom_ref(node.component),
+                dependencies=_PdmBuilder.__get_dependencies_for(node.component)
+            )
+
+    @staticmethod
+    def __get_dependencies_for(component: ComponentInfo) -> Iterable[Dependency]:
+        for _, dependency in component.all_dependencies():
+            yield Dependency(
+                ref=_PdmBuilder.__component_to_bom_ref(dependency.component),
+                dependencies=_PdmBuilder.__get_dependencies_for(dependency.component)
+            )
